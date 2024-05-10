f11994e59f31e2d4c2a03e23dde8cd23d509f0b3
kentoshiohara <a.mode.night.show@gmail.com>
2024-05-10 11:36:02 +0900

    post api実装

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.post('/booklog', (req, res) => {
+    const booklog = req.body
+
+    if (!(booklog.name && booklog.text)) {
+        return res.json({
+            "ok": false,
+            "error": "invalid parameter"
+        })
+    }
+
+    res.json({
+        "ok": true,
+        "booklog" : booklog
+    })
+
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
